42c5de6aa75f90ee98bf9037a954a7102641a676
shenjinxiang <sjx-sword@163.com>
2020-03-26 06:51:49 +0800

    添加 Spring-Boot-MyBatis-Druid

---
+++ java/springboot/Spring-Boot-MyBatis-Druid/src/main/java/com/shenjinxiang/spb/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.shenjinxiang.spb.controller;
+
+import com.shenjinxiang.spb.domain.Student;
+import com.shenjinxiang.spb.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/")
+    public List<Student> queryAll() {
+        return this.studentService.queryAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student queryById(@PathVariable("id") int id) {
+        return this.studentService.queryById(id);
+    }
+
+    @PostMapping(value = "/")
+    public int add(@RequestBody Student student) {
+        return this.studentService.add(student);
+    }
+
+    @PutMapping(value = "/{id}")
+    public int update(@PathVariable int id, @RequestBody Student student) {
+        return this.studentService.update(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public int delById(@PathVariable("id") int id) {
+        return this.studentService.delById(id);
+    }
+}
