1e909dee9bd7dedd0ad4962f28cecd87326c091d
shenjinxiang <sjx-sword@163.com>
2020-04-02 23:04:29 +0800

    d

--- java/springboot/Spring-Boot-MyBatis-MultiSource/src/main/java/com/shenjinxiang/spb/Application.java
+++ java/springboot/Spring-Boot-MyBatis-MultiSource/src/main/java/com/shenjinxiang/spb/Application.java
@@ -2,11 +2,13 @@ package com.shenjinxiang.spb;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Author: ShenJinXiang
  * @Date: 2020/4/2 16:21
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
