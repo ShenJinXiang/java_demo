09442c43199f019cd8ffaa92de406ecf5c73c986
shenjinxiang <sjx-sword@163.com>
2020-04-04 07:06:21 +0800

    开始Spring-Boot-MyBatis-MultiSource-Druid

---
+++ java/springboot/Spring-Boot-MyBatis-MultiSource-Druid/src/main/java/com/shenjinxiang/spb/Application.java
@@ -0,0 +1,16 @@
+package com.shenjinxiang.spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: ShenJinXiang
+ * @Date: 2020/4/4 07:03
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
